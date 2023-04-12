7b9a50e7b38041fc05a1eab65ad8a7ca442a4ce1
Tracker-Friendly <109815155+Tracker-Friendly@users.noreply.github.com>
2023-04-12 12:31:49 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -67,7 +67,8 @@ until [ "$ARCH" = 'amd64' ] || [ "$ARCH" = 'i686' ] || [ "$ARCH" = 'aarch64' ]; 
 done
 
 # Install dependencies to build Achilles
-sudo pacman -Sy wget debootstrap mtools xorriso ca-certificates curl usbutils gcc make gzip xz unzip
+apt-get update
+apt-get install -y --no-install-recommends wget debootstrap mtools xorriso ca-certificates curl libusb-1.0-0-dev gcc make gzip xz-utils unzip libc6-dev
 
 # Get proper files for amd64 or i686
 if [ "$ARCH" = 'amd64' ]; then
