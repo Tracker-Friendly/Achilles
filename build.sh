ffaa95d79c135ad20398bf432d652617ef2711c5
Johnathon Li <jliwin98@gmail.com>
2023-04-14 16:28:42 +0000

    Display Manager test 2

--- build.sh
+++ build.sh
@@ -81,7 +81,7 @@ sleep 2
 cat << ! | chroot rootfs /usr/bin/env PATH=/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin /bin/sh
 apk update
 apk upgrade
-apk add bash alpine-base usbmuxd ncurses udev openssh-client sshpass newt wireless-tools iwd iputils iproute2
+apk add bash alpine-base usbmuxd ncurses udev openssh-client sshpass newt wireless-tools iwd iputils iproute2 wayfire
 apk add --no-scripts linux-lts linux-firmware-none
 rc-update add bootmisc
 rc-update add hwdrivers
